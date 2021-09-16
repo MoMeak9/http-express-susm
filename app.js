27a61d2b2f28d71c5f9fc1b58bf1e45b9824c000
MoMeak9 <1308994506@qq.com>
2021-09-17 00:40:56 +0800

    feat: new struct,eslint

--- app.js
+++ app.js
@@ -8,43 +8,13 @@ const {PRIVATE_KEY, whitelist} = require('./config/constant');
 const routers = require('./routes');
 
 app.use(cors());
-app.use('/', routers);
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 // eslint-disable-next-line no-undef
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(expressJWT({
-    secret: PRIVATE_KEY, // algorithms: ['RS256'],
-    algorithms: ['HS256']
-}).unless({
-    path: whitelist //⽩名单,除了这⾥写的地址，其他的URL都需要验证
-}));
-
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-    console.log(err);
-    if (err.name === 'UnauthorizedError') {
-        // 这个需要根据⾃⼰的业务逻辑来处理
-        res.status(401).send({code: -1, msg: 'token验证失败'});
-    } else {
-        // set locals, only providing error in development
-        res.locals.message = err.message;
-        res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-        // render the error page
-        res.status(err.status || 500);
-        res.render('error');
-    }
-});
-
+app.use('/', routers);
 
 app.listen(9000, () => {
     console.log('Server start on http://localhost:9000');
---
+++ services/userService.js
@@ -0,0 +1,34 @@
+const querySql = require('../db/index');
+const {PWD_SALT, PRIVATE_KEY, EXPIRES} = require('../config/constant');
+const {md5} = require('../uitls');
+const {v4: uuidv4} = require('uuid');
+const jwt = require('jsonwebtoken');
+
+async function login(req, res, next) {
+    let {studentID, password} = req.body;
+    try {
+        let user = await querySql('select * from users where studentID = ?', [studentID]);
+        password = md5(password + PWD_SALT);
+        if (!user || user.length === 0) {
+            res.send({code: -1, msg: "该账户不存在"});
+        } else {
+            let result = await querySql('select * from users where studentID = ? and password = ?', [studentID, password]);
+            if (!result || result.length === 0) {
+                res.send({code: -1, msg: "密码不正确"});
+                let token = jwt.sign({
+                    uuid: result[0].uuid,
+                    op: result[0].op
+                }, PRIVATE_KEY, {expiresIn: EXPIRES});
+                res.send({code: 1, msg: "登入成功", token: `Bearer ${token}`});
+            }
+        }
+    } catch (err) {
+        res.status(404).send({code: -1, msg: "系统异常"});
+        console.log(err);
+        next(err);
+    }
+}
+
+module.exports = {
+    login
+};
