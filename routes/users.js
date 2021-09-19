4c3fae76e61c61992a16ecf6da3d1fc6fb845b7a
MoMeak9 <1308994506@qq.com>
2021-09-19 19:56:34 +0800

    refactor(userRouter):重构服务器路由与服务模块，并分层

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const querySql = require('../db/index');
-const {PWD_SALT, PRIVATE_KEY, EXPIRES, serverAddress} = require('../config/constant');
-const {md5, upload} = require('../uitls');
-const {v4: uuidv4} = require('uuid');
+const {upload} = require('../uitls');
 const service = require('../services/userService');
 
 // 更改为主要负责路由以及路由文档
@@ -111,25 +108,7 @@ router.post('/login', service.login);
  *       '500':
  *         description: 系统异常
  */
-router.post('/register', async (req, res, next) => {
-    let {studentID, password, realName, role, department} = req.body
-    try {
-        let user = await querySql('select * from users where studentID = ?', [studentID])
-        if (!user || user.length === 0) {
-            let uuid = uuidv4()
-            password = md5(password + PWD_SALT)
-            await querySql('insert into users(uuid,studentID,password,realName,role,department,createTime) value(?,?,?,?,?,?,NOW())',
-                [uuid, studentID, password, realName, role, department])
-            res.send({code: 1, msg: "注册成功"})
-        } else {
-            res.send({code: -1, msg: "该账户已存在"})
-        }
-    } catch (err) {
-        res.status(404).send({code: -1, msg: "系统异常"})
-        console.log(err)
-        next(err)
-    }
-});
+router.post('/register', service.register);
 
 // 个人资料查询 自带更新一次个人总分
 /**
@@ -164,16 +143,7 @@ router.post('/register', async (req, res, next) => {
  *       '500':
  *         description: 系统异常
  */
-router.get('/getUserInfo', async (req, res, next) => {
-    let {uuid} = req.user
-    try {
-        let userinfo = await querySql('select DATE_FORMAT(createTime,"%Y-%m-%d %H:%i:%s") AS createTime,No,age,studentID,department,score,grade,ranking,role from users where uuid = ?', [uuid])
-        res.send({code: 1, msg: "成功", data: userinfo[0]})
-    } catch (err) {
-        console.log(err)
-        next(err)
-    }
-});
+router.get('/getUserInfo', service.getUserInfo);
 
 // 查询所有用户信息
 /**
@@ -208,20 +178,7 @@ router.get('/getUserInfo', async (req, res, next) => {
  *       '500':
  *         description: 系统异常
  */
-router.get('/getAllUser', async (req, res, next) => {
-    let {op} = req.user
-    if (op === 1) {
-        try {
-            let userinfo = await querySql('select DATE_FORMAT(createTime,"%Y-%m-%d %H:%i:%s") AS createTime,No,age,studentID,department,score,grade,ranking,role,uuid from users')
-            res.send({code: 1, msg: "成功", data: userinfo})
-        } catch (err) {
-            console.log(err)
-            next(err)
-        }
-    } else {
-        res.send({code: -1, msg: "无权限"})
-    }
-});
+router.get('/getAllUser', service.getAllUser);
 
 // 修改成员状态，删除成员等
 /**
@@ -257,42 +214,7 @@ router.get('/getAllUser', async (req, res, next) => {
  *       '500':
  *         description: 系统异常
  */
-router.post('/updateUser', async (req, res, next) => {
-    let {
-        realName,
-        age,
-        updateUser,
-        department,
-        grade,
-        ranking,
-        role,
-        intro,
-        opLevel,
-        userUuid
-    } = req.body
-    let {op, uuid} = req.user
-    if (op === 1 && uuid !== userUuid) {
-        //    修改他人信息
-        try {
-            await querySql('update users set realName = ?,age = ?,updateUser = ?,updateTime = NOW(),department = ?,grade = ?,ranking = ?,role = ?,intro = ?,op = ? where uuid = ?',
-                [realName, age, updateUser, department, grade, ranking, role, intro, opLevel, userUuid])
-            res.send({code: 1, msg: '更新成功', data: null})
-        } catch (err) {
-            console.log(err)
-            next(err)
-        }
-    } else {
-        //    修改自己信息
-        try {
-            await querySql('update users set realName = ?,age = ?,updateUser = ?,updateTime = NOW(),department = ?,score = ?,grade = ?,ranking = ?,role = ?,intro = ?,op = ? where uuid = ?',
-                [realName, age, updateUser, department, grade, ranking, role, intro, opLevel, uuid])
-            res.send({code: 1, msg: '更新成功', data: null})
-        } catch (err) {
-            console.log(err)
-            next(err)
-        }
-    }
-})
+router.post('/updateUser', service.updateUser);
 
 // 文件上传
 /**
@@ -333,16 +255,7 @@ router.post('/updateUser', async (req, res, next) => {
  *       '500':
  *         description: 系统异常
  */
-router.post('/uploadImage', upload.single('img'), async (req, res, next) => {
-    let {uuid} = req.user
-    if (uuid != null || uuid !== '') {
-        let imgPath = req.file.path.split('public')[1]
-        let imgUrl = serverAddress + imgPath
-        res.send({code: 1, msg: '上传成功', data: {imgUrl: imgUrl, name: req.file.originalname}})
-    } else {
-        res.send({code: -1, msg: '未登入'})
-    }
-});
+router.post('/uploadImage', upload.single('img'), service.uploadImage);
 
 module.exports = router;
 
--- services/userService.js
+++ services/userService.js
@@ -1,7 +1,8 @@
 const querySql = require('../db/index');
-const {PWD_SALT, PRIVATE_KEY, EXPIRES} = require('../config/constant');
+const {PWD_SALT, PRIVATE_KEY, EXPIRES, serverAddress} = require('../config/constant');
 const {md5} = require('../uitls');
 const jwt = require('jsonwebtoken');
+const {v4: uuidv4} = require('uuid');
 
 async function login(req, res, next) {
     let {studentID, password} = req.body;
@@ -30,6 +31,107 @@ async function login(req, res, next) {
     }
 }
 
+async function register(req, res, next) {
+    let {studentID, password, realName, role, department} = req.body;
+    try {
+        let user = await querySql('select * from users where studentID = ?', [studentID]);
+        if (!user || user.length === 0) {
+            let uuid = uuidv4();
+            password = md5(password + PWD_SALT);
+            await querySql('insert into users(uuid,studentID,password,realName,role,department,createTime) value(?,?,?,?,?,?,NOW())',
+                [uuid, studentID, password, realName, role, department]);
+            res.send({code: 1, msg: "注册成功"});
+        } else {
+            res.send({code: -1, msg: "该账户已存在"});
+        }
+    } catch (err) {
+        res.status(404).send({code: -1, msg: "系统异常"});
+        console.log(err);
+        next(err);
+    }
+}
+
+async function getUserInfo(req, res, next) {
+    let {uuid} = req.user;
+    try {
+        let userinfo = await querySql('select DATE_FORMAT(createTime,"%Y-%m-%d %H:%i:%s") AS createTime,No,age,studentID,department,score,grade,ranking,role from users where uuid = ?', [uuid]);
+        res.send({code: 1, msg: "成功", data: userinfo[0]});
+    } catch (err) {
+        console.log(err);
+        next(err);
+    }
+}
+
+async function getAllUser(req, res, next) {
+    let {op} = req.user;
+    if (op === 1) {
+        try {
+            let userinfo = await querySql('select DATE_FORMAT(createTime,"%Y-%m-%d %H:%i:%s") AS createTime,No,age,studentID,department,score,grade,ranking,role,uuid from users');
+            res.send({code: 1, msg: "成功", data: userinfo});
+        } catch (err) {
+            console.log(err);
+            next(err);
+        }
+    } else {
+        res.send({code: -1, msg: "无权限"});
+    }
+}
+
+async function updateUser(req, res, next) {
+
+    let {
+        realName,
+        age,
+        updateUser,
+        department,
+        grade,
+        ranking,
+        role,
+        intro,
+        opLevel,
+        userUuid
+    } = req.body;
+    let {op, uuid} = req.user;
+    if (op === 1 && uuid !== userUuid) {
+        //    修改他人信息
+        try {
+            await querySql('update users set realName = ?,age = ?,updateUser = ?,updateTime = NOW(),department = ?,grade = ?,ranking = ?,role = ?,intro = ?,op = ? where uuid = ?',
+                [realName, age, updateUser, department, grade, ranking, role, intro, opLevel, userUuid]);
+            res.send({code: 1, msg: '更新成功', data: null});
+        } catch (err) {
+            console.log(err);
+            next(err);
+        }
+    } else {
+        //    修改自己信息
+        try {
+            await querySql('update users set realName = ?,age = ?,updateUser = ?,updateTime = NOW(),department = ?,score = ?,grade = ?,ranking = ?,role = ?,intro = ?,op = ? where uuid = ?',
+                [realName, age, updateUser, department, grade, ranking, role, intro, opLevel, uuid]);
+            res.send({code: 1, msg: '更新成功', data: null});
+        } catch (err) {
+            console.log(err);
+            next(err);
+        }
+    }
+}
+
+async function uploadImage(req, res) {
+    let {uuid} = req.user;
+    if (uuid != null || uuid !== '') {
+        let imgPath = req.file.path.split('public')[1];
+        let imgUrl = serverAddress + imgPath;
+        res.send({code: 1, msg: '上传成功', data: {imgUrl: imgUrl, name: req.file.originalname}});
+    } else {
+        res.send({code: -1, msg: '未登入'});
+    }
+}
+
+
 module.exports = {
-    login
+    login,
+    register,
+    getUserInfo,
+    getAllUser,
+    updateUser,
+    uploadImage
 };
